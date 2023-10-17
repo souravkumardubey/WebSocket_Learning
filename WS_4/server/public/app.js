a76fb8e80cd65a864ece5b8f1e275f66aaa5a3a8
souravkumardubey <skdlp2000@gmail.com>
2023-10-17 20:46:45 +0530

    initial commit

---
+++ WS_4/server/public/app.js
@@ -0,0 +1,35 @@
+const socket = io("ws://localhost:3500");
+const activity = document.querySelector(".activity");
+const msgInput = document.querySelector("input");
+
+const sendMessage = (e) => {
+  e.preventDefault();
+  if (msgInput.value) {
+    socket.emit("message", msgInput.value);
+    msgInput.value = "";
+  }
+  msgInput.focus();
+};
+
+document.querySelector("form").addEventListener("submit", sendMessage);
+
+// listen for messages
+socket.on("message", (data) => {
+  activity.textContent = "";
+  const li = document.createElement("li");
+  li.textContent = data;
+  document.querySelector("ul").appendChild(li);
+});
+
+msgInput.addEventListener("keypress", () => {
+  socket.emit("activity", socket.id.substring(0, 5));
+});
+
+let activityTimeout;
+socket.on("activity", (name) => {
+  activity.textContent = `${name} is typing...`;
+  clearTimeout(activityTimeout);
+  activityTimeout = setTimeout(() => {
+    activity.textContent = "";
+  }, 1000);
+});
